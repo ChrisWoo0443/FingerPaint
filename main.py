aad71ac280ebc16bc8f5756d2de962f7a72fae4b
ChrisWoo0443 <chefwoodini@gmail.com>
2025-05-12 15:27:06 -0700

    changed webcam to be browser based sent through websockets so webapp can be deployed

--- main.py
+++ main.py
@@ -11,6 +11,10 @@ from websockets.exceptions import ConnectionClosed
 import asyncio
 import uvicorn
 
+import base64
+from io import BytesIO
+from PIL import Image
+
 '''
 https://www.youtube.com/watch?v=1H9qUzmSm_M
 websocket tutorial
@@ -19,13 +23,6 @@ websocket tutorial
 app = FastAPI()
 app.mount("/style", StaticFiles(directory="style"), name="style")
 templates = Jinja2Templates(directory="template")
-
-
-'''
-change camera_val to your the value used by your webcam
-'''
-camera_val = 1
-camera = cv2.VideoCapture(camera_val)
 
 
 mpHands = mp.solutions.hands
@@ -52,180 +49,115 @@ touched = False
 def index(request: Request):
     return templates.TemplateResponse('index.html', {"request": request})
 
-@app.websocket('/canvas')
-async def get_canvas(websocket: WebSocket):
+
+@app.websocket("/process")
+async def process_stream(websocket: WebSocket):
     await websocket.accept()
     try:
-        draw_mode = False
-        touched = False
         while True:
-            success, frame = camera.read()
-            if not success:
-                break
-            else:
-                
-                frame = cv2.flip(frame, 1)
-                canvas = np.full(frame.shape, 255, dtype="uint8")
+            data = await websocket.receive_text()
+            header, encoded = data.split(",", 1)
+            img_bytes = base64.b64decode(encoded)
 
-                Process = hands.process(frame)  # process the image
-                landmarkList = []
+            img = Image.open(BytesIO(img_bytes)).convert("RGB")
+            frame = cv2.cvtColor(np.array(img), cv2.COLOR_RGB2BGR)
+            frame = cv2.flip(frame, 1)
 
-                # Draw hand landmarks
-                if Process.multi_hand_landmarks:
-                    for handlm in Process.multi_hand_landmarks:
-                        for _id, landmarks in enumerate(handlm.landmark):
-                            height, width, color_channels = frame.shape
-                            x, y = int(landmarks.x * width), int(landmarks.y * height)
-                            landmarkList.append([_id, x, y])
-                        Draw.draw_landmarks(canvas, handlm, mpHands.HAND_CONNECTIONS)
+            # Process with Mediapipe
+            Process = hands.process(frame)
+            if Process.multi_hand_landmarks:
+                for handlm in Process.multi_hand_landmarks:
+                    Draw.draw_landmarks(frame, handlm, mpHands.HAND_CONNECTIONS)
 
-                if landmarkList:
-                    # Get the coordinates of the landmarks
-                    # x_0, y_0 = landmarkList[0][1], landmarkList[0][2]       # palm
-                    x_1, y_1 = landmarkList[4][1], landmarkList[4][2]       # tips of thumb
-                    x_2, y_2 = landmarkList[8][1], landmarkList[8][2]       # tips of index
-                    x_3, y_3 = landmarkList[12][1], landmarkList[12][2]     # tips of middle
-                    # x_4, y_4 = landmarkList[16][1], landmarkList[16][2]     # tips of ring
-                    # x_5, y_5 = landmarkList[20][1], landmarkList[20][2]     # tips of pinky
-                
-                     # Draw circles on the tips of each finger
-                    cv2.circle(canvas, (x_1, y_1), 7, (0, 255, 0), cv2.FILLED)      # thumb
-                    cv2.circle(canvas, (x_2, y_2), 7, (0, 0, 0), cv2.FILLED)      # index
-                    cv2.circle(canvas, (x_3, y_3), 7, (0, 255, 0), cv2.FILLED)      # middle
-
-                    TI = hypot(x_2-x_1, y_2-y_1)        # thumb and index
-                    interTI = np.interp(TI, [15,220], [0,100])
-                    TM = hypot(x_3-x_1, y_3-y_1)        # thumb and middle
-                    interTM = np.interp(TM, [15, 220], [0, 100])
-
-                    # clear the canvas by touching thumb to pointer
-                    if int(interTI) < 20:
-                        myPoints.clear()
-
-                    # toggle drawing mode by touching thumb to middle finger
-                    if int(interTM) < 20 and not touched:
-                        old_draw = draw_mode
-                        draw_mode = not draw_mode
-                        if old_draw:
-                            myPoints.append(None)
-                        touched = True
-                    elif int(interTM) > 30:
-                        touched = False
-
-                    # Draw if in drawing mode
-                    if draw_mode:
-                        myPoints.append((x_2, y_2, (0, 0, 0), circleRadius))
-
-                # smoother drawing instead of drawing dots
-                if len(myPoints) >= 2:
-                    for i in range(1, len(myPoints)):
-                        if myPoints[i - 1] is None or myPoints[i] is None:
-                            continue
-                        pt1 = myPoints[i - 1][:2]
-                        pt2 = myPoints[i][:2]
-                        cv2.line(canvas, pt1, pt2, (0, 0, 0), circleRadius)
-
-                _, buffer = cv2.imencode('.jpg', canvas)
-
-                await websocket.send_bytes(buffer.tobytes())
+            _, buffer = cv2.imencode('.jpg', frame)
+            base64_img = base64.b64encode(buffer).decode("utf-8")
+            await websocket.send_text(f"data:image/jpeg;base64,{base64_img}")
             await asyncio.sleep(0.01)
     except (WebSocketDisconnect, ConnectionClosed):
         print("Client disconnected")
 
 
-
-
-@app.websocket('/webcam')
-async def get_stream(websocket: WebSocket):
+@app.websocket('/canvas')
+async def canvas_from_client(websocket: WebSocket):
     await websocket.accept()
-    try: 
-        draw_mode = False
-        touched = False
+    draw_mode = False
+    touched = False
+    try:
         while True:
-            success, frame = camera.read()
-            if not success:
-                break
-            else:
-                
-                frame = cv2.flip(frame, 1)
+            data = await websocket.receive_text()
+            _, encoded = data.split(",", 1)
+            img_bytes = base64.b64decode(encoded)
 
-                Process = hands.process(frame)  # process the image
-                landmarkList = []
+            img = Image.open(BytesIO(img_bytes)).convert("RGB")
+            frame = cv2.cvtColor(np.array(img), cv2.COLOR_RGB2BGR)
+            frame = cv2.flip(frame, 1)
 
-                # Draw hand landmarks
-                if Process.multi_hand_landmarks:
-                    for handlm in Process.multi_hand_landmarks:
-                        for _id, landmarks in enumerate(handlm.landmark):
-                            height, width, color_channels = frame.shape
-                            x, y = int(landmarks.x * width), int(landmarks.y * height)
-                            landmarkList.append([_id, x, y])
-                        Draw.draw_landmarks(frame, handlm, mpHands.HAND_CONNECTIONS)
+            canvas = np.full(frame.shape, 255, dtype="uint8")
+            Process = hands.process(frame)
+            landmarkList = []
 
-                if landmarkList:
-                    # Get the coordinates of the landmarks
-                    # x_0, y_0 = landmarkList[0][1], landmarkList[0][2]       # palm
-                    x_1, y_1 = landmarkList[4][1], landmarkList[4][2]       # tips of thumb
-                    x_2, y_2 = landmarkList[8][1], landmarkList[8][2]       # tips of index
-                    x_3, y_3 = landmarkList[12][1], landmarkList[12][2]     # tips of middle
-                    # x_4, y_4 = landmarkList[16][1], landmarkList[16][2]     # tips of ring
-                    # x_5, y_5 = landmarkList[20][1], landmarkList[20][2]     # tips of pinky
-                
-                     # Draw circles on the tips of each finger
-                    # cv2.circle(frame, (x_0, y_0), 7, (0, 255, 0), cv2.FILLED)
-                    cv2.circle(frame, (x_1, y_1), 7, (0, 255, 0), cv2.FILLED)       # thumb
-                    cv2.circle(frame, (x_2, y_2), 7, (0, 0, 0), cv2.FILLED)       # index
-                    cv2.circle(frame, (x_3, y_3), 7, (0, 255, 0), cv2.FILLED)       # middle
-                    # cv2.circle(frame, (x_4, y_4), 7, (0, 255, 0), cv2.FILLED)
-                    # cv2.circle(frame, (x_5, y_5), 7, (0, 255, 0), cv2.FILLED)
+            if Process.multi_hand_landmarks:
+                for handlm in Process.multi_hand_landmarks:
+                    for _id, landmarks in enumerate(handlm.landmark):
+                        h, w, _ = frame.shape
+                        x, y = int(landmarks.x * w), int(landmarks.y * h)
+                        landmarkList.append([_id, x, y])
+                    Draw.draw_landmarks(canvas, handlm, mpHands.HAND_CONNECTIONS)
 
-                    TI = hypot(x_2-x_1, y_2-y_1)        # thumb and index
-                    interTI = np.interp(TI, [15,220], [0,100])
-                    # MP = hypot(x_3-x_0, y_3-y_0)        # middle and palm
-                    # interMP = np.interp(MP, [15,300], [0,100])
-                    # RP = hypot(x_4-x_0, y_4-y_0)        # ring and palm
-                    # interRP = np.interp(RP, [15,300], [0,100])
-                    # PP = hypot(x_5-x_0, y_5-y_0)        # pinky and palm
-                    # interPP = np.interp(PP, [15,300], [0,100])
-                    TM = hypot(x_3-x_1, y_3-y_1)        # thumb and middle
-                    interTM = np.interp(TM, [15, 220], [0, 100])
+            if landmarkList:
+                # Get the coordinates of the landmarks
+                # x0, y0 = landmarkList[0][1], landmarkList[0][2]       # palm
+                x1, y1 = landmarkList[4][1], landmarkList[4][2]       # tips of thumb
+                x2, y2 = landmarkList[8][1], landmarkList[8][2]       # tips of index
+                x3, y3 = landmarkList[12][1], landmarkList[12][2]     # tips of middle
+                # x4, y4 = landmarkList[16][1], landmarkList[16][2]     # tips of ring
+                # x5, y5 = landmarkList[20][1], landmarkList[20][2]     # tips of pinky
 
-                    # clear the canvas by touching thumb to pointer
-                    if int(interTI) < 20:
-                        myPoints.clear()
+                # Draw circles on the tips of each finger
+                cv2.circle(canvas, (x1, y1), 7, (0, 255, 0), cv2.FILLED)      # thumb
+                cv2.circle(canvas, (x2, y2), 7, (0, 0, 0), cv2.FILLED)      # index
+                cv2.circle(canvas, (x3, y3), 7, (0, 255, 0), cv2.FILLED)      # middle
 
-                    # toggle drawing mode by touching thumb to middle finger
-                    if int(interTM) < 20 and not touched:
-                        old_draw = draw_mode
-                        draw_mode = not draw_mode
-                        if old_draw:
-                            myPoints.append(None)
-                        touched = True
-                    elif int(interTM) > 30:
-                        touched = False
+                TI = hypot(x2 - x1, y2 - y1)        # thumb and index
+                TM = hypot(x3 - x1, y3 - y1)        # thumb and middle
+                interTI = np.interp(TI, [15, 220], [0, 100])
+                interTM = np.interp(TM, [15, 220], [0, 100])
 
-                    # Draw if in drawing mode
-                    if draw_mode:
-                        myPoints.append((x_2, y_2, (0, 0, 0), circleRadius))
+                # clear the canvas by touching thumb to pointer
+                if int(interTI) < 20:
+                    myPoints.clear()
 
-                # smoother drawing instead of drawing dots
-                # if len(myPoints) >= 2:
-                #     for i in range(1, len(myPoints)):
-                #         if myPoints[i - 1] is None or myPoints[i] is None:
-                #             continue
-                #         pt1 = myPoints[i - 1][:2]
-                #         pt2 = myPoints[i][:2]
-                #         cv2.line(frame, pt1, pt2, (0, 0, 0), circleRadius)
+                # toggle drawing mode by touching thumb to middle finger
+                if int(interTM) < 20 and not touched:
+                    draw_mode = not draw_mode
+                    if not draw_mode:
+                        myPoints.append(None)
+                    touched = True
+                elif int(interTM) > 30:
+                    touched = False
 
-                # for points in myPoints:
-                #     cv2.circle(canvas, (points[0], points[1]), points[3], points[2], cv2.FILLED)
-                #     cv2.circle(frame, (points[0], points[1]), points[3], points[2], cv2.FILLED)
+                # Draw if in drawing mode
+                if draw_mode:
+                    myPoints.append((x2, y2, (0, 0, 0), circleRadius))
 
-                _, buffer = cv2.imencode('.jpg', frame)
 
-                await websocket.send_bytes(buffer.tobytes())
+            # smoother drawing instead of drawing dots
+            if len(myPoints) >= 2:
+                for i in range(1, len(myPoints)):
+                    if myPoints[i - 1] is None or myPoints[i] is None:
+                        continue
+                    pt1 = myPoints[i - 1][:2]
+                    pt2 = myPoints[i][:2]
+                    cv2.line(canvas, pt1, pt2, (0, 0, 0), circleRadius)
+
+            _, buffer = cv2.imencode('.jpg', canvas)
+            base64_img = base64.b64encode(buffer).decode("utf-8")
+            await websocket.send_text(f"data:image/jpeg;base64,{base64_img}")
             await asyncio.sleep(0.01)
+
     except (WebSocketDisconnect, ConnectionClosed):
         print("Client disconnected")
+
 
 
 if __name__ == '__main__':
