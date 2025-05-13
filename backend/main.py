0c50eb34bf70aa6aa65347c88f11880c735fd0e3
ChrisWoo0443 <chefwoodini@gmail.com>
2025-05-13 09:24:40 -0700

    changed directories

--- backend/main.py
+++ backend/main.py
@@ -161,5 +161,5 @@ async def canvas_from_client(websocket: WebSocket):
 
 
 if __name__ == '__main__':
-    uvicorn.run(app, host = '127.0.0.1', port = 8000)
-
+    # uvicorn.run(app, host = '127.0.0.1', port = 8000)
+    uvicorn.run(app, host = '0.0.0.0', port = 10000)
